d38bc280435f26ef8ad9d7c3e901c223eb6e64cc
eduardovallejo <vallejoprietoeduardo@gmail.com>
2016-03-14 09:08:27 -0100

    tercer commit xavi
    1 no he aconseguit configurar lo de les routes sorry

--- src/MVC/ModuleBundle/MVCModuleBundle.php
+++ src/MVC/ModuleBundle/MVCModuleBundle.php
@@ -2,8 +2,19 @@
 
 namespace MVC\ModuleBundle;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class MVCModuleBundle extends Bundle
 {
+	/**
+	 * @Route("/info")
+	 */
+	public function infoAction()
+	{
+	    // replace this example code with whatever you need
+	    return $this->render('default/index.html.twig');
+	}
 }
